e39f74a31d5e647e6180cf12f9296d2e29c38d70
יוני טרמי <yonitermi@gmail.com>
2024-01-07 13:12:42 +0200

    docker hub credentials works

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -1,7 +1,7 @@
 module "eks" {
     source  = "terraform-aws-modules/eks/aws"
     version = "~> 19.0"
-    cluster_name = "recruit-cluster"
+    cluster_name = "recruiters-cluster"
     cluster_version = "1.24"
 
     cluster_endpoint_public_access  = true
